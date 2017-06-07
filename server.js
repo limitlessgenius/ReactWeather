112daf745f8df61b1adf217186626f9c328c55c8
josemanuel1 <j.arago@factoryandfacts.com>
2017-06-07 16:45:31 +0200

    Deployment Adjustments

--- server.js
+++ server.js
@@ -3,8 +3,32 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
     console.log(`Express Server On Port: ${PORT}`);
 });
+
+// var express = require('express');
+
+// var app = express();
+// const PORT = process.env.PORT || 4000;
+
+// app.use(express.static('public'));
+
+// app.listen(PORT, function() {
+//     console.log(`Express Server On Port: ${PORT}`);
+// });
+
+
+
+
+
